d37932913d9bca594a48fd1c985d756787801b64
Derek Miranda <derek.miranda@ff0000.com>
2017-12-01 13:05:16 -0800

    Added other ES6 deps

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import Async from './lib/async.js'
+import Async from './lib/Async.js'
+import CssManager from './lib/CssManager.js'
+import ImageManager from './lib/ImageManager.js'
+import Expandable from './lib/Expandable.js'
+import ExpandableDcs from './lib/ExpandableDcs.js'
+import PrepareCore from './lib/PrepareCore.js'
 
 // expose the lib packages as modules
 // TODO: Is there a better way to do this ????
 export {
-	Async
+	Async,
+	CssManager,
+	ImageManager,
+	Expandable,
+	ExpandableDcs,
+	PrepareCore,
 }
