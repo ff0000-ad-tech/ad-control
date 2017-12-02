182952a0939fcf61392836a120097761b3123c99
Derek Miranda <derek.miranda@ff0000.com>
2017-12-01 17:47:57 -0800

    Prepare* imports

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+import PrepareCore from './lib/PrepareCore.js'
+import PrepareCommon from './lib/PrepareCommon.js'
 import Async from './lib/Async.js'
 import CssManager from './lib/CssManager.js'
 import ImageManager from './lib/ImageManager.js'
 import Expandable from './lib/Expandable.js'
 import ExpandableDcs from './lib/ExpandableDcs.js'
-import PrepareCore from './lib/PrepareCore.js'
 
 // expose the lib packages as modules
 // TODO: Is there a better way to do this ????
 export {
+	PrepareCore,
+	PrepareCommon,
 	Async,
 	CssManager,
 	ImageManager,
 	Expandable,
 	ExpandableDcs,
-	PrepareCore,
 }
