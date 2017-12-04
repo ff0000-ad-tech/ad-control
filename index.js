feaa305b2c70ee2a3c576f356939902b9a90fb17
Derek Miranda <derek.miranda@derekmi-macpro.ff0000.com>
2017-12-04 14:39:28 -0800

    Moved prepare stuff to build.Control

--- index.js
+++ index.js
@@ -6,27 +6,7 @@ import ImageManager from './lib/ImageManager.js'
 import Expandable from './lib/Expandable.js'
 import ExpandableDcs from './lib/ExpandableDcs.js'
 
-// expose the lib packages as modules
 // TODO: Is there a better way to do this ????
-
-// expose this prepare hook to the payloader in the index script
-// TODO: is there also a better way of doing this?
-window.prepareCoreCommonAndBuild = function(fbaContent) {
-	function prepareCore() {
-		trace('\n\n\n -- Payloader.prepareCore() --')
-		PrepareCore.init(prepareCommon, fbaContent)
-	}
-	function prepareCommon() {
-		trace('\n\n\n -- Payloader.prepareCommon() --')
-		PrepareCommon.init(prepareBuild)
-	}	
-	function prepareBuild() {
-		trace('\n\n\n -- Payloader.prepareBuild() --')
-		Control.prepareBuild()
-	}
-	prepareCore();
-}
-
 export {
 	PrepareCore,
 	PrepareCommon,
