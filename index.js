f2377c63cecdcbc41f6d5855b1dca760046c9fa9
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 11:35:37 -0800

    Sheet and ViewManagers

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import PrepareCommon from './lib/PrepareCommon.js'
 import Async from './lib/Async.js'
 import CssManager from './lib/CssManager.js'
 import ImageManager from './lib/ImageManager.js'
+import SheetManager from './lib/SheetManager.js'
+import ViewManager from './lib/ViewManager.js'
 import Expandable from './lib/Expandable.js'
 import ExpandableDcs from './lib/ExpandableDcs.js'
-// import LiveScoring from './lib/LiveScoring.js'
+import LiveScoring from './lib/LiveScoring.js'
 
 // TODO: Is there a better way to do this ????
 export {
@@ -14,7 +16,9 @@ export {
 	Async,
 	CssManager,
 	ImageManager,
+	SheetManager,
+	ViewManager,
 	Expandable,
 	ExpandableDcs,
-	// LiveScoring,
+	LiveScoring,
 }
