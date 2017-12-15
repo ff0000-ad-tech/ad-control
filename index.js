8c91079f212bc356e5faeb198ad7bfd29b3d109c
Derek Miranda <derek.miranda@ff0000.com>
2017-12-15 12:17:57 -0800

    export default from

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-import PrepareCore from './lib/PrepareCore.js'
-import PrepareCommon from './lib/PrepareCommon.js'
-import Async from './lib/Async.js'
-import CssManager from './lib/CssManager.js'
-import ImageManager from './lib/ImageManager.js'
-import SheetManager from './lib/SheetManager.js'
-import ViewManager from './lib/ViewManager.js'
-import Expandable from './lib/Expandable.js'
-import ExpandableDcs from './lib/ExpandableDcs.js'
-import LiveScoring from './lib/LiveScoring.js'
-
-// TODO: Is there a better way to do this ????
-export {
-	PrepareCore,
-	PrepareCommon,
-	Async,
-	CssManager,
-	ImageManager,
-	SheetManager,
-	ViewManager,
-	Expandable,
-	ExpandableDcs,
-	LiveScoring,
-}
+export { default as PrepareCore } from './lib/PrepareCore.js'
+export { default as PrepareCommon } from './lib/PrepareCommon.js'
+export { default as Async } from './lib/Async.js'
+export { default as CssManager } from './lib/CssManager.js'
+export { default as ImageManager } from './lib/ImageManager.js'
+export { default as SheetManager } from './lib/SheetManager.js'
+export { default as ViewManager } from './lib/ViewManager.js'
+export { default as Expandable } from './lib/Expandable.js'
+export { default as ExpandableDcs } from './lib/ExpandableDcs.js'
+export { default as LiveScoring } from './lib/LiveScoring.js'
