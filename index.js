be92cc8fd72d54916df378ece0880a01d8ba593c
Greg Connell <gconnell@160over90.com>
2021-06-28 16:58:14 -0700

    fixing the imports

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as Expandable from './lib/Expandable.js'
-import * as ExpandableDcs from './lib/ExpandableDcs.js'
+import ExpandableDcs from './lib/ExpandableDcs.js'
 import YouTube from './lib/YouTube'
+
 export { Expandable, ExpandableDcs, YouTube }
